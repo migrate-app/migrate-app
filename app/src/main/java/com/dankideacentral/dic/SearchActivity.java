bc53193cd25e933591f6d60a4b2ccbaa07feb0de
Chris Ermel <ermel272@gmail.com>
2016-10-02 22:41:50 -0400

    Adding initial search activity shell

---
+++ app/src/main/java/com/dankideacentral/dic/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.dankideacentral.dic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Class:   SearchActivity.java
+ * Purpose: Activity to search for
+ */
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
