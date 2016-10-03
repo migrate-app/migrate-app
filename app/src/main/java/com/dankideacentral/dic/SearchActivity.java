e087c34939c52920db7c1d7a8d3398be177e5e56
Chris Ermel <ermel272@gmail.com>
2016-10-02 22:55:55 -0400

    fixing a missing javadoc

--- app/src/main/java/com/dankideacentral/dic/SearchActivity.java
+++ app/src/main/java/com/dankideacentral/dic/SearchActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 
 /**
  * Class:   SearchActivity.java
- * Purpose: Activity to search for
+ * Purpose: Activity to search for a geographical location
+ *          or to enable location services for the app
+ *
+ * @author Chris Ermel
+ * @version 1.0
+ * @since 2016-10-2
  */
 public class SearchActivity extends AppCompatActivity {
 
