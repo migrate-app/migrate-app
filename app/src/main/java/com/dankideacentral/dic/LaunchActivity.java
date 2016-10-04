265d19117da79668c1fc86886427aad02c8a55a9
Alex <amaxwell@Alexanders-MacBook-Pro.local>
2016-10-03 20:42:05 -0400

    Dynamic launch functionality

---
+++ app/src/main/java/com/dankideacentral/dic/LaunchActivity.java
@@ -0,0 +1,39 @@
+package com.dankideacentral.dic;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String twitterAuth = preferences.getString(getString(R.string.twitter_auth_preference), "");
+        String fineLocationPermission = "android.permission.ACCESS_FINE_LOCATION";
+        PackageManager pm = this.getPackageManager();
+        int hasFineLocationPermission = pm.checkPermission(
+                fineLocationPermission,
+                this.getPackageName());
+        // auth tokens don't expire. Just check if it exists
+        if ("".equals(twitterAuth)) {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        } else {
+            if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED) {
+                Intent mapIntent = new Intent(this, MainActivity.class);
+                startActivity(mapIntent);
+            } else {
+                Intent searchIntent = new Intent(this, SearchActivity.class);
+                startActivity(searchIntent);
+            }
+        }
+    }
+
+
+    }
