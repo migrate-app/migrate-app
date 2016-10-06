0249c3d064992ebed115d4f8ef0eae71f5279b43
Basim Ramadhan <basim.ramadhan@gmail.com>
2016-10-06 10:32:36 -0400

    Started work

---
+++ app/src/main/java/com/dankideacentral/dic/ConstantValues.java
@@ -0,0 +1,16 @@
+package com.dankideacentral.dic;
+
+/**
+ * Created by basim on 10/6/16.
+ */
+
+public class ConstantValues {
+    public static String TWITTER_CONSUMER_KEY = "SArN2YtMrnaCgenNghXb0NKRC";
+    public static String TWITTER_CONSUMER_SECRET = "S45LXQjBFbzb7PFC1UOvmeUs4WTzMjOpjnsMyIb3FG13oGqXO0";
+    public static String TWITTER_CALLBACK_URL = "oauth://com.dankideacentral.dic";
+    public static String URL_PARAMETER_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
+    public static String PREFERENCE_TWITTER_OAUTH_TOKEN="TWITTER_OAUTH_TOKEN";
+    public static String PREFERENCE_TWITTER_OAUTH_TOKEN_SECRET="TWITTER_OAUTH_TOKEN_SECRET";
+    public static String PREFERENCE_TWITTER_IS_LOGGED_IN="TWITTER_IS_LOGGED_IN";
+    public static String STRING_EXTRA_AUTHENCATION_URL = "AuthencationUrl";
+}
--- app/src/main/java/com/dankideacentral/dic/LoginActivity.java
+++ app/src/main/java/com/dankideacentral/dic/LoginActivity.java
@@ -3,11 +3,86 @@ package com.dankideacentral.dic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Copied from the tutorial
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import com.hintdesk.core.activities.AlertMessageBox;
+import com.hintdesk.core.util.OSUtil;
+import com.hintdesk.core.util.StringUtil;
+import twitter4j.auth.RequestToken;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private boolean isUseStoredTokenKey = false;
+    private boolean isUseWebViewForAuthentication = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeLoginButton();
+
+        /* If the user is already authenticated, log the user in */
+        if (isUseStoredTokenKey) {
+            logIn();
+        }
+    }
+
+    /* Attach click listener to the login button */
+    private void initializeLoginButton() {
+        View.OnClickListener buttonLoginOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn();
+            }
+        };
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(buttonLoginOnClickListener);
+    }
+
+    private void logIn() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        if (!sharedPreferences.getBoolean(ConstantValues.PREFERENCE_TWITTER_IS_LOGGED_IN, false)) {
+            new TwitterAuthenticateTask().execute();
+        } else {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    class TwitterAuthenticateTask extends AsyncTask<String, String, RequestToken> {
+        @Override
+        protected void onPostExecute(RequestToken requestToken) {
+            if (requestToken!=null)
+            {
+                if (!isUseWebViewForAuthentication)
+                {
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(requestToken.getAuthenticationURL()));
+                    startActivity(intent);
+                }
+                else
+                {
+                    Intent intent = new Intent(getApplicationContext(), OAuthActivity.class);
+                    intent.putExtra(ConstantValues.STRING_EXTRA_AUTHENCATION_URL,requestToken.getAuthenticationURL());
+                    startActivity(intent);
+                }
+            }
+        }
+
+        @Override
+        protected RequestToken doInBackground(String... params) {
+            return TwitterUtil.getInstance().getRequestToken();
+        }
     }
 }
