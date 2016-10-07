f0d18aa97d2f30c6daab631ce3fd2f08f8f3ff2b
Basim Ramadhan <basim.ramadhan@gmail.com>
2016-10-06 23:59:13 -0400

    Improved login screen UI

--- app/src/main/java/com/dankideacentral/dic/authentication/OAuthWebViewFragment.java
+++ app/src/main/java/com/dankideacentral/dic/authentication/OAuthWebViewFragment.java
@@ -53,7 +53,7 @@ public class OAuthWebViewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.oauth_webview,container,false);
+        View view = inflater.inflate(R.layout.fragment_oauth_webview,container,false);
         webView = (WebView)view.findViewById(R.id.webViewOAuth);
         return view;
     }
