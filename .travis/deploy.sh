b27d57b8724995adb04c83cc18883161e181a735
Seena Rowhani <seenarowhani95@gmail.com>
2016-10-19 22:10:53 -0400

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,22 @@
-./gradlew assembleRelease -Pandroid.injected.signing.store.file=.credentials/key.jks -Pandroid.injected.signing.store.password=$STORE_PASSWORD -Pandroid.injected.signing.key.alias=migrate -Pandroid.injected.signing.key.password=$STORE_PASSWORD
-github-release release --user dankideacentral --repo migrate-app --tag v$(cat .version) --name $(cat .version) --description "Built by your friendly neighbourhood Travis CI"
-github-release upload  --user dankideacentral --repo migrate-app  --tag v$(cat .version) --name "migrate-release-$(date +%Y-%m-%d)-v$(cat .version).apk" --file "app/build/outputs/apk/app-release.apk"
+#!/bin/bash
+set -ev
+bundle exec rake:units
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  ./gradlew assembleRelease \
+     -Pandroid.injected.signing.store.file=.credentials/key.jks \
+     -Pandroid.injected.signing.store.password=$STORE_PASSWORD \
+     -Pandroid.injected.signing.key.alias=migrate \
+     -Pandroid.injected.signing.key.password=$STORE_PASSWORD
+  github-release release \
+    --user dankideacentral \
+    --repo migrate-app \
+    --tag v$(cat .version) \
+    --name $(cat .version) \
+    --description "Built by your friendly neighbourhood Travis CI"
+  github-release upload  \
+    --user dankideacentral \
+    --repo migrate-app  \
+    --tag v$(cat .version) \
+    --name "migrate-release-$(date +%Y-%m-%d)-v$(cat .version).apk" \
+    --file "app/build/outputs/apk/app-release.apk"
+fi
